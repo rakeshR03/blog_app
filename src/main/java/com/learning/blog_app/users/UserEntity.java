d2c24055e525193303d0ede742633e2616c71043
Rakesh <rakeshroy03@outlook.com>
2022-10-09 21:29:50 +0530

    Adding controllers, services and repositories

--- src/main/java/com/learning/blog_app/users/UserEntity.java
+++ src/main/java/com/learning/blog_app/users/UserEntity.java
@@ -1,15 +1,50 @@
 package com.learning.blog_app.users;
 
+import com.learning.blog_app.articles.ArticleEntity;
 import com.learning.blog_app.common.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.lang.Nullable;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity(name="users")
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserEntity extends BaseEntity {
+    @Column( nullable = false, unique = true)
+    String username;
+
+    @Column( nullable = false)@Setter
+    String password;
+
+    @Column( nullable = false, unique = true)
+    String email;
+
+    @Nullable   @Setter
+    String bio;
+
+    @Nullable   @Setter
+    String image;
+
+    @OneToMany(mappedBy = "author", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    Set<ArticleEntity> authoredArticles;
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "likers")
+    Set<ArticleEntity> likedArticle;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+            @JoinTable(
+                    name = "followers",
+                    joinColumns = @JoinColumn(name = "following_id"),
+                    inverseJoinColumns = @JoinColumn(name="follower_id")
+            )
+    Set<UserEntity> followers;
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "followers")
+    Set<UserEntity> following;
 }
